ffb898e181968f50857b2a5a31e808506ba95a7c
Rawad Hossain <151036956+rawad-hossain@users.noreply.github.com>
2024-02-13 01:16:21 +0600

    Time: 16 ms (51.15%) | Memory: 20.3 MB (24.18%) - LeetSync

---
+++ 136-single-number/single-number.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int n = nums.size();
+        sort(nums.begin(), nums.end());
+        if (n == 1) {
+            return nums[0];
+        }
+        for (int i = 0; i < n - 1; i += 2) {
+            if (nums[i] != nums[i + 1]) {
+                return nums[i];
+            }
+            if (nums[n - 1] != nums[n - 2]) {
+                return nums[n - 1];
+            }
+        }
+        return 0;
+    }
+};
