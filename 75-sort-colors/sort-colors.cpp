d46399a0c461ca7fed7d1b1022bdd0677f3ac1ba
Rawad Hossain <151036956+rawad-hossain@users.noreply.github.com>
2024-02-13 01:55:32 +0600

    Time: 5 ms (13.21%) | Memory: 9.8 MB (14.24%) - LeetSync

---
+++ 75-sort-colors/sort-colors.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    void sortColors(vector<int>& arr) {
+        int n = arr.size();
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (arr[i] > arr[j]) 
+                    swap(arr[i], arr[j]);
+            }
+        }
+    }
+};
