e3c77a8cfb8488b2afc2a1143ddd68f90b3290b1
Rawad Hossain <151036956+rawadhossain@users.noreply.github.com>
2024-02-15 12:39:58 +0600

    brute-force-approach.cpp

--- 128-longest-consecutive-sequence/brute-force-approach.cpp
+++ 128-longest-consecutive-sequence/brute-force-approach.cpp
@@ -1,1 +1,28 @@
+class Solution {
+public:
+    bool linearSearch(vector<int>& arr, int k) {
+        for (int i = 0; i < arr.size(); i++) {
+            if (arr[i] == k)
+                return true;
+        }
+        return false;
+    }
 
+    int longestConsecutive(vector<int>& arr) {
+        int n = arr.size();
+        if (n == 0)
+            return 0;
+
+        int length = 1, x;
+        for (int i = 0; i < n; i++) {
+            x = arr[i];
+            int count = 1;
+            while (linearSearch(arr, x + 1) == true) {
+                x++;
+                count++;
+            }
+            length = max(length, count);
+        }
+        return length;
+    }
+};
