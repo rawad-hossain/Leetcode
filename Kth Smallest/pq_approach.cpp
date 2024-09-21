1163f6b2c245557f675062a4d2e093d4d4193567
Rawad Hossain <rawad.hossain00@gmail.com>
2024-09-21 13:06:17 +0600

    pq_approach.cpp

---
+++ Kth Smallest/pq_approach.cpp
@@ -0,0 +1,20 @@
+class Solution {
+  public:
+    // arr : given array
+    // k : find kth smallest element and return using this function
+    int kthSmallest(vector<int> &arr, int k)
+    {
+        priority_queue<int, vector<int>, greater<int>> pq;
+
+        int n = arr.size();
+        for (int i = 0; i < n; i++)
+        {
+            pq.push(arr[i]);
+        }
+
+        while (pq.size() != n - k + 1)
+            pq.pop();
+
+        return pq.top();
+    }
+};
